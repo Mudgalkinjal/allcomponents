47d4f276cbbe8dda4d862ff1cfd65c887a6f2cf4
Kinjal Mudgal <mudgalkinjal@gmail.com>
2023-12-19 17:17:13 -0500

    Created dynamic grid

--- src/components/Games/Tictactoe.js
+++ src/components/Games/Tictactoe.js
@@ -1,20 +1,69 @@
 
 import '../../styles/Games.css';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Tictactoe() {
-    const [ grid, setGrid ] = useState(3)
+    const [grid, setGrid] = useState(3)
+    const [gridItems, setGridItems] = useState([]);
+    const [toggleElem, setToggleElem ] = useState('O')
+    const numRows = grid;
+    const numCols = grid;
 
     function handleInputChange(event) {
         setGrid(event.target.value)
     }
-    console.log(grid)
+
+    function handleClick(e) {
+        const clickedItem = e.target;
+        console.log(toggleElem);
+        clickedItem.textContent = toggleElem;
+        setToggleElem('X')
+        // setToggleElem((prevToggleElem) => (prevToggleElem === 'O' ? 'X' : 'O'));
+      }
+      
+
+      // Generate the grid dynamically
+  const generateGrid = () => {
+    const items = [];
+    for (let row = 1; row <= numRows; row++) {
+      for (let col = 1; col <= numCols; col++) {
+        // Create a unique key for each grid item
+        const key = `item-${row}-${col}`;
+
+        // Assign class names for rows and columns
+        const classNames = `grid-item row-${row} col-${col}`;
+
+        // Set some content (you can customize this as needed)
+        const content = ``;
+
+        // Push the JSX element for the grid item into the array
+        items.push(
+          <div key={key} className={classNames} onClick={handleClick}>
+            {content}
+          </div>
+        );
+      }
+    }
+    // Update the state with the new grid items
+    setGridItems(items);
+  };
+
+  // Call the generateGrid function when the component mounts
+  React.useEffect(() => {
+    generateGrid();
+  }, [numRows, numCols]);
+    
 
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            <label htmlFor='gridNum'>Enter the number of grids you want for Tic Tac Toe: </label>
-            <input min={3} onChange={handleInputChange} id="gridNum" type="number" />
+            <div id="inputGridClass">
+                <label>Enter the number of grids you want: </label>
+                <input onChange={handleInputChange} min={3} max={6} type='number' />
+            </div>
+            <div className="grid-container" style={{gridTemplateColumns:`repeat(${grid},0fr)`}}>
+                {gridItems}
+            </div>
         </div>
     )
 }
