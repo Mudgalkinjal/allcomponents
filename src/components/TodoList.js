3e43491e109e45539a0343cd9f0dc5c031afebec
Kinjal Mudgal <mudgalkinjal@gmail.com>
2023-12-25 19:40:14 -0500

    Completed todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/App.css';
 
 export default function TodoList() {
-
     const [list, setList] = useState([]);
+    const [msg, setMsg] = useState('');
+
+    useEffect(() => {
+        console.log(list); // Log the updated state when it changes
+    }, [list]);
 
     function handleList(event) {
         if (event.key === 'Enter') {
-            setList((prevList)=>[...prevList, event.target.value])
+            let msg = checkForDup(event.target.value);
+            if (msg === 'Task added') {
+                setList((prevList) => [...prevList, { task: event.target.value, completed: false }]);
+            }
+            setMsg(msg);
         }
     }
 
-    return (<div>
-        <div id="toDoContainer">
-            <label htmlFor='taskId'>Enter the task:</label>
-            <input id="taskId" type="text" onKeyDown={handleList} />
+    function checkForDup(textI) {
+        const stringWithoutSpaces = textI.replace(/\s/g, '');
+        const isDuplicate = list.some(item => item.task === stringWithoutSpaces);
+        if (isDuplicate) {
+            return 'Task already in the list';
+        } else {
+            return 'Task added';
+        }
+    }
+
+    function handleStrike(strInd) {
+        setList((prevList) =>
+            prevList.map((item, index) =>
+                index === strInd ? { ...item, completed: true } : item
+            )
+        );
+    }
+
+    function handleDelete(delInd) {
+        setList((prevList) =>
+            prevList.filter((item, index)=>index!==delInd)
+        );
+    }
+
+    return (
+        <div>
+            <div id="toDoContainer">
+                <label htmlFor='taskId'>Enter the task:</label>
+                <input id="taskId" type="text" onKeyDown={handleList} />
+                <span>{msg}</span>
+            </div>
+            <div id="listingTasks">
+                {list.map((elem, index) => (
+                    <ul onClick={() => handleStrike(index)} key={index}>
+                        <li style={{ textDecoration: elem.completed ? 'line-through' : 'none' }}>
+                            {elem.task}
+                        </li>
+                        <button onClick={()=>handleDelete(index)} id="secondary-Btn">Delete</button>
+                    </ul>
+                ))}
+            </div>
         </div>
-        <div id="listingTasks">
-            {list.map((elem, index) => (
-            <ul key={index}>
-                <li>{elem}</li>
-            </ul>
-            ))}
-        </div>
-    </div>)
+    );
 }
