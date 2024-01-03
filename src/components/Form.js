71d44a8c24a87f3aa91805558cbddee090985467
Kinjal Mudgal <mudgalkinjal@gmail.com>
2024-01-02 19:30:05 -0500

    form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,36 @@
 import '../styles/App.css';
 
 export default function Form() {
-    return (<div id="formComponent">Form</div>)
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(e.target.elements.name.value)
+        console.log(e.target.elements.phone.value)
+        console.log(e.target.elements.age.value)
+        console.log(e.target.elements.address.value)
+    }
+
+    return (<div id="formComponent">
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor='text'>Enter the name: </label>
+                <input type="text" id="name" name="name" />
+            </div>
+            <div>
+                <label htmlFor='phone'>Enter the phone: </label>
+                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" id="phone" />
+            </div>
+            <div>
+                <label htmlFor='age'>Enter the age: </label>
+                <input type="number"  id="age" name="age" max={100} />
+            </div>
+            <div>
+                <label htmlFor='address'>Enter the address: </label>
+                <textarea type="textArea" name="address" id="address" />
+            </div>
+            <div>
+                <button type='submit'>Submit</button>
+            </div>
+        </form>
+    </div>)
 }
